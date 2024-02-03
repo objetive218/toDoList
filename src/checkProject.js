58de4000259784053d0a7191239e46f36ff383e2
objetive218 <davidsantiagotc@gmail.com>
2024-02-03 17:11:43 -0500

    idea for check firts charge

--- src/checkProject.js
+++ src/checkProject.js
@@ -1,16 +1,8 @@
-export default function checkProject(name) {
-  switch (name) {
-    case "All":
-      // create without button to create
-      break;
-    case "Today":
-      // create without button to create
-      break;
-    case "This week":
-      // create without button to create
-      break;
-    default:
-      //create with the name project
-      break;
-  }
+export default function checkProject(main) {
+  if (JSON.parse(localStorage.getItem("arrayTask"))) {
+      listTask("default");
+      printTask("All Task");
+    } else {
+      
+    }
 }
