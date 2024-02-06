17ddc7c77386121a3c2ebe4087d0a7ef48d9d73f
objetive218 <davidsantiagotc@gmail.com>
2024-02-06 16:21:09 -0500

    clean notes

--- src/checkProject.js
+++ src/checkProject.js
@@ -1,5 +1,4 @@
 import printTask from "./addTask";
-import { listTask } from "./listTask";
 import { projects } from "./projects";
 
 export default function checkProject() {
@@ -7,40 +6,10 @@ export default function checkProject() {
     JSON.parse(localStorage.getItem("arrayTask")) &&
     JSON.parse(localStorage.getItem("arrayProjects"))
   ) {
-    //idMain.appendChild(createListTask());
-    //listTask("default");
     printTask("All Task");
-    projects();
+    projects(true);
   } else {
-    //idMain.appendChild(createListTask());
     ("");
   }
 }
 
-// export function createProject() {
-//   let create = document.createElement("form");
-//   create.innerHTML = `<input type="text" placeholder="Enter project name" id="newProject">
-//         `;
-//   //asign to button function projects()
-//   let btnc = document.createElement("button");
-//   let btnd = document.createElement("button");
-//   btnc.setAttribute("id", "createP");
-//   btnc.textContent = "Create";
-//   btnd.setAttribute("id", "cancelP");
-//   btnd.textContent = "Cancel";
-
-//   create.appendChild(btnc);
-//   create.appendChild(btnd);
-//   create.setAttribute("id", "boxCreate");
-//   create.setAttribute("class", "active");
-//   btnc.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     projects();
-//     document.getElementById("boxCreate").classList.toggle("active");
-//   });
-//   btnd.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     document.getElementById("boxCreate").classList.toggle("active");
-//   });
-//   return create;
-// }
